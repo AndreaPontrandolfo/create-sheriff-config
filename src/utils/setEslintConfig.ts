31cb54c80d13018eb158685ddc2075867bff8ce4
AndreaPontrandolfo <andrea.pontra@gmail.com>
2022-10-10 18:33:37 +0200

    feat(ignores): added warning if eslintignore is found

--- src/utils/setEslintConfig.ts
+++ src/utils/setEslintConfig.ts
@@ -16,20 +16,33 @@ export default [
 
 export const setEslintConfig = async () => {
   const ESLINT_CONFIG_FILE_NAME = 'eslint.config.js';
+  const ESLINT_IGNORE_FILE_NAME = '.eslintignore';
+
   try {
     const eslintConfigFile = await findUp(ESLINT_CONFIG_FILE_NAME);
+    const eslintIgnoreFile = await findUp(ESLINT_IGNORE_FILE_NAME);
+
+    if (eslintIgnoreFile) {
+      printWarning(
+        `A ${ESLINT_IGNORE_FILE_NAME} file was found. Please remove it and transfer the ignored files list to the ${ESLINT_CONFIG_FILE_NAME} 'ignores' array.`,
+      );
+    }
+
     if (eslintConfigFile) {
       logger.verbose(
         `'${ESLINT_CONFIG_FILE_NAME}' file found. Skipping '${ESLINT_CONFIG_FILE_NAME}' file generation and configuration.`,
       );
       return;
     }
+
     logger.verbose(
       `'${ESLINT_CONFIG_FILE_NAME}' file not found. Generating and configuring '${ESLINT_CONFIG_FILE_NAME}' file...`,
     );
+
     printWarning(
       'If you have other Eslint configs in your project, remove them',
     );
+
     try {
       writeFileSync(ESLINT_CONFIG_FILE_NAME, eslintConfigRawText);
       printSucces(`Successfully generated ${ESLINT_CONFIG_FILE_NAME} file`);
